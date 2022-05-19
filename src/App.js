80432a13d6a6bc59f83e6c1d7bb84fb2d4e3a01b
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-05-19 20:01:28 +0530

    included state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import covid from "./covid.json";
+
+Object.entries(covid).map((idx) => console.log(idx[0]));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Object.entries(covid).map((idx) => (
+        <h1>{idx[0]}</h1>
+      ))}
     </div>
   );
 }
